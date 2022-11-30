5af229f6bc699f3c12b0c7baef3768f72b280efd
SssngM <rarang222@gmail.com>
2022-11-30 09:53:49 -0800

    update responsive

--- src/components/productlist/productlist.js
+++ src/components/productlist/productlist.js
@@ -16,51 +16,55 @@ export const Productlist = () => {
   return (
     <div className="pl">
       <h3 className="port"> PROJECTS </h3>
-
-      <div className="p-list">
-        <Product img={wireframing} />
-        <div className="p-des">
-          <h2 className="p-title"> "Tech Girls Social" </h2>
-          <div> with Drawing, Figma, React. </div>
-          <div> building this website </div>
-          <div> as ux/ui and front-end role.</div>
-          <a href="https://www.figma.com/file/RpLX82xFYIxrRlCgbRuanJ/tech-girls-social-wireframing?node-id=0%3A1">
-            {" "}
-            Figma Mockup
-          </a>
-          <div>
-            <a href="https://techgirlssocial.netlify.app"> Tech Girls Social</a>
+      <div className="products">
+        <div className="p-list">
+          <Product img={wireframing} />
+          <div className="p-des">
+            <h2 className="p-title"> "Tech Girls Social" </h2>
+            <div> with Drawing, Figma, React. </div>
+            <div> building this website </div>
+            <div> as ux/ui and front-end role.</div>
+            <a href="https://www.figma.com/file/RpLX82xFYIxrRlCgbRuanJ/tech-girls-social-wireframing?node-id=0%3A1">
+              {" "}
+              Figma Mockup
+            </a>
+            <div>
+              <a href="https://techgirlssocial.netlify.app">
+                {" "}
+                Tech Girls Social
+              </a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="p-list">
-        <div className="p-des">
-          <h2 className="p-title"> "Sweet Store Project" </h2>
-          <div>with Javscript, React.</div>
-          <div> Router,Boostrap,Cart funtions</div>
-          <a href="https://your-sweets.netlify.app" rel="noopener noreferrer">
-            {" "}
-            site{" "}
-          </a>
+        <div className="p-list">
+          <div className="p-des">
+            <h2 className="p-title"> "Sweet Store Project" </h2>
+            <div>with Javscript, React.</div>
+            <div> Router,Boostrap,Cart funtions</div>
+            <a href="https://your-sweets.netlify.app" rel="noopener noreferrer">
+              {" "}
+              site{" "}
+            </a>
+          </div>
+          <Product img={sweet} />
         </div>
-        <Product img={sweet} />
-      </div>
-      <div className="p-list">
-        <Product img={todolist} />
+        <div className="p-list">
+          <Product img={todolist} />
 
-        <div className="p-des">
-          <h2 className="p-title"> "To Do List Project" </h2>
-          <div>with Javscript, React.</div>
-          <div>log in, add, edit, delete funtions</div>
+          <div className="p-des">
+            <h2 className="p-title"> "To Do List Project" </h2>
+            <div>with Javscript, React.</div>
+            <div>log in, add, edit, delete funtions</div>
 
-          <a
-            href="https://github.com/SssngM/TodoList-2022"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            github code{" "}
-          </a>
+            <a
+              href="https://github.com/SssngM/TodoList-2022"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              github code{" "}
+            </a>
+          </div>
         </div>
       </div>
     </div>
